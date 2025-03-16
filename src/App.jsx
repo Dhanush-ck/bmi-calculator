df538656906482ab8ef957118f3555ebe4c1f79d
Dhanush CK <dhanushck01@gmail.com>
2025-03-16 17:14:41 +0530

    feat : Added category field

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,35 @@ function App() {
 
   const [weight, setWeight] = useState();
   const [height, setHeight] = useState();
-  const [bmi, setBMI] = useState();
+  const [bmi, setBMI] = useState(NaN);
+  const [category, setCategory] = useState("Enter the details");
 
   function calculate() {
     const val = weight / (height * height);
     setBMI(val.toFixed(1));
+    if(val){
+      if(val < 18.5){
+        setCategory("Underweight");
+      }
+      else if(val < 24.9) {
+        setCategory("Normal weight");
+      }
+      else if(val < 29.9) {
+        setCategory("Overweight");
+      }
+      else if(val < 34.9) {
+        setCategory("Obesity I");
+      }
+      else if(val < 39.9) {
+        setCategory("Obesity II");
+      }
+      else {
+        setCategory("Severe Obesity(Obesity III)");
+      }
+    }
+    else {
+      setCategory("Enter the details")
+    }
   }
 
   return(
@@ -23,10 +47,17 @@ function App() {
           <div className="height">
             <input type="text" className="input-bar" placeholder="Enter your height in cm" onChange={(e)=>{setHeight(e.target.value/100)}}/>
           </div>
+          <button className="submit-button" onClick={calculate}>
+            <div>Submit</div>
+          </button>
         </div>
-        <button className="submit-button" onClick={calculate}>Submit</button>
         <div className="output">
-          {bmi}
+          <div className="bmi">
+            {bmi}
+          </div>
+          <div className="category">
+            {category}
+          </div>
         </div>
       </div>
     </div>
